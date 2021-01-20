21b65575b92348e9dae82c4034bea0fbf92d86fa
Enrique <jruvalcaba1@ucol.mx>
2021-01-20 12:07:15 -0600

    Ya quedó

--- src/app/detallecliente/detallecliente.page.ts
+++ src/app/detallecliente/detallecliente.page.ts
@@ -1,27 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { NavParams } from '@ionic/angular';
-import { HttpClient } from '@angular/common/http';
-import { ModalController } from '@ionic/angular';
-import { AlertController } from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import { NavController, NavParams } from "@ionic/angular";
+import { HttpClient } from "@angular/common/http";
+import { ModalController } from "@ionic/angular";
+import { AlertController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-detallecliente',
-  templateUrl: './detallecliente.page.html',
-  styleUrls: ['./detallecliente.page.scss'],
+  selector: "app-detallecliente",
+  templateUrl: "./detallecliente.page.html",
+  styleUrls: ["./detallecliente.page.scss"],
 })
 export class DetalleclientePage implements OnInit {
   respuesta: any;
   item: any;
 
-
-  constructor(public navParams: NavParams,
-              public http: HttpClient,
-              public modalController: ModalController,
-              public alertController: AlertController) {
-                this.item = navParams.get('dato');}
-
-  ngOnInit() {
+  constructor(
+    public navParams: NavParams,
+    public http: HttpClient,
+    public modalController: ModalController,
+    public alertController: AlertController,
+    public navCtrl: NavController
+  ) {
+    this.item = navParams.get("dato");
   }
+
+  ngOnInit() {}
 
   actualizarCliente(item) {
     // const uri = 'https://bdpromo1.000webhostapp.com/api.php?comando=editar&nombre=' + item.nombre
@@ -32,54 +34,65 @@ export class DetalleclientePage implements OnInit {
     // '&fotografia=' + item.fotografia +
     // '&id=' + item.id;
 
-    const uri = 'https://appinventor2020.000webhostapp.com/tienda_api/clientes.php?comando=editar&id='+item.id+'&nombre='
-    +item.nombre+'&direccion='
-    +item.direccion+'&telefono='
-    +item.telefono+'&correo='
-    +item.correo;
+    const uri =
+      "https://appinventor2020.000webhostapp.com/tienda_api/clientes.php?comando=editar&id=" +
+      item.id +
+      "&nombre=" +
+      item.nombre +
+      "&direccion=" +
+      item.direccion +
+      "&telefono=" +
+      item.telefono +
+      "&correo=" +
+      item.correo;
 
-    this.http.get(uri).subscribe(async data => {
-      this.respuesta = data;
-      const mensaje = this.respuesta.mensaje;
-      if (!!mensaje) {
-      {
+    this.http.get(uri).subscribe(async (data) => {
+      const res: any = data;
+      this.respuesta = res.mensaje;
+
+      if (this.respuesta != "") {
         const alert = await this.alertController.create({
-          header: 'Alerta',
-          subHeader: 'Actualizar',
-          message: mensaje,
-          buttons: ['OK']
+          header: "Cliente actualizado",
+          message: "El cliente se actualizó con éxito.",
+          buttons: ["OK"],
         });
         await alert.present();
         this.modalController.dismiss();
+        this.navCtrl.navigateBack("/clientes");
+      } else {
+        const alert = await this.alertController.create({
+          header: "Error al actualizar",
+          message: "No fue posible actualizar al cliente.",
+          buttons: ["OK"],
+        });
+        await alert.present();
       }
-    }
-
-  });
+    });
   }
 
-   eliminarCliente(id) {
-    const uri = 'https://appinventor2020.000webhostapp.com/tienda_api/clientes.php?comando=eliminar&id='+id;
-    this.http.get(uri).subscribe(async data => {
+  eliminarCliente(id) {
+    const uri =
+      "https://appinventor2020.000webhostapp.com/tienda_api/clientes.php?comando=eliminar&id=" +
+      id;
+    this.http.get(uri).subscribe(async (data) => {
       this.respuesta = data;
       const mensaje = this.respuesta.mensaje;
       if (!!mensaje) {
-      {
-        const alert = await this.alertController.create({
-          header: 'Alerta',
-          subHeader: 'Eliminar',
-          message: mensaje,
-          buttons: ['OK']
-        });
-        await alert.present();
-        this.modalController.dismiss();
+        {
+          const alert = await this.alertController.create({
+            header: "Alerta",
+            subHeader: "Eliminar",
+            message: mensaje,
+            buttons: ["OK"],
+          });
+          await alert.present();
+          this.modalController.dismiss();
+        }
       }
-    }
-
-  });
+    });
   }
 
-   regresar() {
+  regresar() {
     this.modalController.dismiss();
-    }
-
+  }
 }
