d187103b2cc7dec3b3273ede48349d85fa8314e2
Enrique <jruvalcaba1@ucol.mx>
2021-01-20 11:01:15 -0600

    Parece que ya jaló todo

--- src/app/agregarcliente/agregarcliente.page.ts
+++ src/app/agregarcliente/agregarcliente.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { ModalController } from "@ionic/angular";
+import {
+  AlertController,
+  ModalController,
+  NavController,
+} from "@ionic/angular";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-agregarcliente",
@@ -13,15 +18,20 @@ export class AgregarclientePage implements OnInit {
   direccion: string;
   telefono: number;
   correo: string;
-
+  usuario: any;
   url = "";
   respuesta: any;
 
   constructor(
     public http: HttpClient,
-    public modalController: ModalController
-  ) {}
-  guardarCliente() {
+    public modalController: ModalController,
+    public navCtrl: NavController,
+    public alertController: AlertController,
+    private router: Router
+  ) {
+    this.usuario = this.router.getCurrentNavigation().extras.state.usuario;
+  }
+  async guardarCliente() {
     // const uri = 'https://bdpromo1.000webhostapp.com/api.php?comando=agregar&nombre=' + this.nombre
     // + '&descripcion=' + this.descripcion
     // + '&preciodecosto=' + this.preciodecosto +
@@ -30,7 +40,9 @@ export class AgregarclientePage implements OnInit {
     // '&fotografia=' + this.url;
 
     const uri =
-      "https://appinventor2020.000webhostapp.com/tienda_api/clientes.php?comando=agregar&idDueno=1&nombre=" +
+      "https://appinventor2020.000webhostapp.com/tienda_api/clientes.php?comando=agregar&idDueno=" +
+      this.usuario.id +
+      "&nombre=" +
       this.nombre +
       "&direccion=" +
       this.direccion +
@@ -39,14 +51,31 @@ export class AgregarclientePage implements OnInit {
       "&correo=" +
       this.correo;
 
-    this.http.get(uri).subscribe((data) => {
-      this.respuesta = data;
-      const mensaje = this.respuesta.mensaje;
+    this.http.get(uri).subscribe(async (data) => {
+      const res: any = data;
+      this.respuesta = res.mensaje;
+
+      if (this.respuesta != "") {
+        const alert = await this.alertController.create({
+          header: "Cliente agregado",
+          message: "El cliente se agregó con éxito.",
+          buttons: ["OK"],
+        });
+        await alert.present();
+        this.navCtrl.navigateBack("/clientes");
+      } else {
+        const alert = await this.alertController.create({
+          header: "Error al agregar",
+          message: "No fue posible agregar al cliente.",
+          buttons: ["OK"],
+        });
+        await alert.present();
+      }
     });
   }
 
   cancelar() {
-    this.modalController.dismiss();
+    this.navCtrl.navigateBack("/clientes");
   }
 
   ngOnInit() {}
